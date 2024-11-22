af2588122c34c6b31d061fdfbd57e1c9126fda92
2Carrot10 <eleanorgrutz@gmail.com>
2024-11-21 18:49:45 -0800

    add wezterm

---
+++ dots/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'JetBrains Mono'
+
+config.color_scheme = 'Tokyo Night'
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+  left = 0,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+
+return config
