7da11678f1eb42032e51248485aef57a6b06b0ff
2Carrot10 <eleanorgrutz@gmail.com>
2025-04-08 22:12:56 -0700

    Move lsp related logic.

--- dots/neovim/nvim/lua/core/options.lua
+++ dots/neovim/nvim/lua/core/options.lua
@@ -36,14 +36,6 @@ vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) 
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
-vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "Gives information about current function"})
-
-vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { desc = "Rename function"})
-
-vim.keymap.set("n", "<leader>ld", vim.lsp.buf.declaration, { desc = "Go to decleration"})
-
-vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { desc = "Go to references"})
-
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
--- dots/neovim/nvim/lua/core/plugins/lsp/lspconfig.lua
+++ dots/neovim/nvim/lua/core/plugins/lsp/lspconfig.lua
@@ -78,11 +78,19 @@ return {
         opts.desc = "Go to next diagnostic"
         keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-        opts.desc = "Show documentation for what is under cursor"
-        keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
         opts.desc = "Restart LSP"
         keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+
+
+        -- TODO: remove duplicates
+        vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "Gives information about current function"})
+
+        vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { desc = "Rename function"})
+
+        vim.keymap.set("n", "<leader>ld", vim.lsp.buf.declaration, { desc = "Go to decleration"})
+
+        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { desc = "Go to references"})
+
       end,
     })
 
